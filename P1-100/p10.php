347b99e0e93735e4fe303f2cdbee7d0ecee8e496
joshfedo <joshfedo@gmail.com>
2017-05-26 09:04:28 -0500

    p10

---
+++ P1-100/p10.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fedo
+ * Date: 5/24/2017
+ * Time: 4:41 PM
+ *
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+ */
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+
+
+
+function find_prime_sum($max = 2000000)
+{
+	$test = 2;
+	$number_line = range(2, $max);
+	while ($test < $max) {
+		for ($i = $test; $i <= $max; $i += $test) {
+			if ($i != $test) {
+				unset($number_line[$i - 2]);
+			}
+		}
+		$test++;
+	}
+	return array_sum($number_line);
+}
+
+echo find_prime_sum();
